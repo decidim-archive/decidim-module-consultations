499e1a82369e60bea46e6be4d75849d1af90d9b3
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2018-01-12 11:23:07 +0100

    Fixed missing methods related to scopable participatory spaces.

--- app/models/concerns/decidim/consultations/override_scopeable.rb
+++ app/models/concerns/decidim/consultations/override_scopeable.rb
@@ -9,12 +9,20 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+        def scopes_enabled?
+          false
+        end
+
         def subscopes
           organization.top_scopes
         end
 
         def has_subscopes?
           subscopes.any?
+        end
+
+        def scope
+          nil
         end
       end
     end
